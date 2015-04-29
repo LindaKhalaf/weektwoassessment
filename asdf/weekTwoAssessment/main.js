dbaa3256dc048b60c180aa4f8dd9e0601aa65ba1
Linda Khalaf <khalaflb24@gmail.com>
2015-04-29 10:51:40 -0700

    this is the week two assessment yay

---
+++ asdf/weekTwoAssessment/main.js
@@ -0,0 +1,15 @@
+// $(document).ready(function(){
+//     $("dan" ).click(function() {
+//        alert("hey you clicked on me!");
+//     });
+//    $("<div class='dan'><p>suuuuuuup</p></div>" ).appendTo(document.body);
+// });
+
+function clicky(){
+$(document).ready(function(){
+    $('.dan' ).click(function() {
+       alert("hey you clicked on me!");
+    });
+   $("<div class='dan'><p>suuuuuuup</p></div>" ).appendTo(document.body);
+});
+}
